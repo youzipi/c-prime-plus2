94df9b4beff8d38de7db7cafa25cac16327ba0e7
youzipi <blake_12@qq.com>
2013-12-15 21:43:04 +0800

    菜单设计0.3
    代码优化：
    1.加入！ isspace（）又回车符扩展为空白符。
    2.解决函数返回值为'\n'的错误。

--- 8-8/menuette.cpp
+++ 8-8/menuette.cpp
@@ -1,24 +1,37 @@
 #include<stdio.h>
+#include<ctype.h>
 
 char get_choice(){
-	char ch;
-	while((ch = getchar()) != '\n'){
-		printf("switch:\n");
+	char ch,c;
+	//ch = getchar();
+	//while((c = getchar()) != '\n'){
+	c = getchar();
+	while(!isspace(c)){
+	//此处不是判断末尾的回车符，而是为了防止用户输入第一个字符前不小心输入了回车符，优化：，改为空白符。
+	//while(ch != '\n'){	//判断前需要先把值读入变量中，结果就是函数的最终的返回值变为'\n'，即循环终止的条件
+		ch = c;
+		printf("ch:%c||",ch);
+		printf("switch:\n");	
 		goto td;
 		td: switch(ch){
 				case 'a':
 				case 'b':
 				case 'c':
-				case 'q':break;
+				case 'q':printf("legal\n");break;
 				default:printf("We just accept a,b,c and q.\n");
 						while(getchar() != '\n')
 							continue;
 						ch = getchar();
 						goto td;
+						
 			}
-			//getchar();
+		//printf("ch:%c|",ch);
+		//ch = getchar();
+		c = getchar();
 	}
+	printf("to main\n");
 	putchar(ch);
+	printf("ch:%c|\n",ch);
 	return ch;
 };
 
@@ -26,7 +39,7 @@ int main(){
 	char choice;
 
 	choice = get_choice();
-	printf("%c",choice);
+	printf("choice:%c|\n",choice);
 	return 0;
 }
 
